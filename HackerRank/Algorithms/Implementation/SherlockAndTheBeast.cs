45452e4d374888a476f23a23f5b3a88db4ead94d
Robson F. da Veiga <robsonfveiga@gmail.com>
2016-05-10 08:11:09 -0300

    SherlockAndTheBeast

--- HackerRank/Algorithms/Implementation/SherlockAndTheBeast.cs
+++ HackerRank/Algorithms/Implementation/SherlockAndTheBeast.cs
@@ -11,23 +11,60 @@ namespace HackerRank.Algorithms.Implementation
     {
         public void run(StreamReader Console)
         {
-            double value,final;
-            string strValue;
+            string three, five,final;
+            int limit;
             int t = Convert.ToInt32(Console.ReadLine());
+
+
             for (int x = 0; x < t; x++)
             {
                 int n = Convert.ToInt32(Console.ReadLine());
-                value = Double.Parse(new String('5', n));
 
-                Lif (value % 3 == 0 || value % 5 == 0) {
-                    System.Console.WriteLine(value);
+                if (n < 3)
+                {
+                    System.Console.WriteLine(-1);
                 }
+                else if (n == 3)
+                {
+                    System.Console.WriteLine("555");
+     
+                }
+                else if (n == 5)
+                {
+                    System.Console.WriteLine("33333");
 
-                for (int xx = 0; xx < n; xx++) {
+                }
+                else
+                {
+                    three = new String('3', n);
+                    final = three;
+                    limit = -1;
+                    for (int xx = 1; xx < n; xx++)
+                    {
+                        if (three.Length != 5)
+                        {
+                            three = three.Remove(0, xx);
+                        }
+                        else {
+                            if(limit==-1)
+                                limit = xx;
+                        }
+
+                        five = new String('5', xx);
                     
+                        if (three.Length % 5 == 0 && five.Length % 3 == 0) {
+                            final = five + three;
+                        }
+                    }
+                    if (limit != -1)
+                    {
+                        System.Console.WriteLine(final.Remove(0, limit - 1));
+                    }
+                    else {
+                        System.Console.WriteLine(final);
+                    }
                 }
             }
-           
         }
     }
 }
