dbe86650613a53044647340c49d191b8bf5a9a6c
Robson F. da Veiga <robsonfveiga@gmail.com>
2016-05-11 17:01:36 -0300

    KthAncestor started

---
+++ HackerRank/Algorithms/GraphTheory/KthAncestor.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HackerRank.Algorithms.GraphTheory
+{
+    class KthAncestor : HackerRank
+    {
+        public void run(StreamReader Console)
+        {
+            int t,p,x,y,q;
+            int[] nodes;
+            t = int.Parse(Console.ReadLine());
+
+            for (int i = 0; i < t; i++) {
+
+                //Numero de nodes
+                p = int.Parse(Console.ReadLine());
+
+                for (int ii = 0; ii < p; ii++) {
+                    nodes = Array.ConvertAll(Console.ReadLine().Split(' '),int.Parse);
+                    //Pai
+                    y = nodes[0];
+                    x = nodes[1];
+                }
+
+                //Numero de consultas
+                q = int.Parse(Console.ReadLine());
+
+            }
+
+        }
+    }
+}
