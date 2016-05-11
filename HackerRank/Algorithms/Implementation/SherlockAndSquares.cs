6bcbe9fbee610820a0d324e4034d037d9871bb31
Robson F. da Veiga <robsonfveiga@gmail.com>
2016-05-11 15:34:42 -0300

    SherlockAndSquares

---
+++ HackerRank/Algorithms/Implementation/SherlockAndSquares.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HackerRank.Algorithms.Implementation
+{
+    class SherlockAndSquares : HackerRank
+    {
+        public void run(StreamReader Console)
+        {
+            int t;
+            Int64[] range;
+            t = Convert.ToInt32(Console.ReadLine());
+            
+            for (int x = 0; x < t; x++)
+            {
+                range = Array.ConvertAll(Console.ReadLine().Split(' '), Int64.Parse);
+                System.Console.WriteLine((Math.Floor(Math.Sqrt(range[1])) - Math.Ceiling(Math.Sqrt(range[0])) + 1));
+            }
+        }
+    }
+}
--- HackerRank/app.cs
+++ HackerRank/app.cs
@@ -15,7 +15,7 @@ namespace HackerRank
         static void Main(String[] args)
         {
             StreamReader input = new StreamReader("input.txt");
-            HackerRank hackerRank = new ClassVsInstance();
+            HackerRank hackerRank = new SherlockAndSquares();
             hackerRank.run(input);
 
             Console.ReadLine();
