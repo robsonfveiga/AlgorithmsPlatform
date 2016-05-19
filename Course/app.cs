649323b1cc9581b4c7ec7063d4c121dfa8c3acaa
Robson Fernando Da Veiga <robsonfveiga@gmail.com>
2016-05-19 06:59:06 -0300

    curso

---
+++ Course/app.cs
@@ -0,0 +1,19 @@
+﻿using Course.DynamicConnectivity;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Course
+{
+    class app
+    {
+        static void Main(String[] args)
+        {
+            Tester test = new Course.DynamicConnectivity.UnionFindTester();
+            test.letsRock();
+        }
+    }
+}
