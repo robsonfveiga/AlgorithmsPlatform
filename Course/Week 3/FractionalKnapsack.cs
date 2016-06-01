48d35cb7238d4ba0bb471254552ec5e8959d64a3
Robson F. da Veiga <robsonfveiga@gmail.com>
2016-06-01 15:08:57 -0300

    FractionalKnapsack.cs

--- Course/Week 3/FractionalKnapsack.cs
+++ Course/Week 3/FractionalKnapsack.cs
@@ -12,20 +12,72 @@ namespace AlgorithmToolbox.Course.Week_3
     {
         public void run(StreamReader Console)
         {
-            int[] values = Array.ConvertAll(Console.ReadLine().Split(' '),int.Parse);
+            int[] values = Array.ConvertAll(Console.ReadLine().Split(' '), int.Parse);
             int numEle = values[0];
             int capacidade = values[1];
-            int valor,peso,unidade;
+            int valor, peso, unidade;
 
-            List<int> quantidade;
-            List<int> quantidadeFinal;
-
-            for (int x = 0; x < numEle; x++) {
+            List<ItemKnapsack> itens = new List<ItemKnapsack>();
+            for (int x = 0; x < numEle; x++)
+            {
                 values = Array.ConvertAll(Console.ReadLine().Split(' '), int.Parse);
                 valor = values[0];
                 peso = values[1];
+                itens.Add(new ItemKnapsack(valor, peso));
+            }
+            itens.Sort();
+
+            while (capacidade != 0) {
+                foreach (ItemKnapsack item in itens) {
+                    
+                }
             }
 
+        }
+    }
+
+    class ItemKnapsack : IComparable
+    {
+        int valor;
+        int peso;
+        public ItemKnapsack(int valor,int peso)
+        {
+            this.peso = peso;
+            this.valor = valor;
+        }
+
+        public int Valor
+        {
+            get
+            {
+                return valor;
+            }
+
+            set
+            {
+                valor = value;
+            }
+        }
+
+        public int Peso
+        {
+            get
+            {
+                return peso;
+            }
+
+            set
+            {
+                peso = value;
+            }
+        }
+
+        public int CompareTo(object obj)
+        {
+            ItemKnapsack that = (ItemKnapsack)obj;
+            if (this.Valor > that.Valor && this.Peso<that.Peso) return -1;
+            if (this.Valor == that.Valor && this.Peso == that.Peso) return 0;
+            return 1;
         }
     }
 }
